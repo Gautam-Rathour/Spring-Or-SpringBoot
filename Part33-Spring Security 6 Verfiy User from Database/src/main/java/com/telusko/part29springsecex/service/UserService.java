fba3768ffeeb03c3a999d3d17e50a5f98b4b5638
Gautam-Rathour <gau321tam@gmail.com>
2026-04-19 12:45:23 +0530

    finally adding so many learning file

--- Part33-Spring Security 6 Verfiy User from Database/src/main/java/com/telusko/part29springsecex/service/UserService.java
+++ Part33-Spring Security 6 Verfiy User from Database/src/main/java/com/telusko/part29springsecex/service/UserService.java
@@ -1,4 +1,19 @@
 package com.telusko.part29springsecex.service;
 
+import com.telusko.part29springsecex.model.Users;
+import com.telusko.part29springsecex.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    public Users register(Users user) {
+        return repo.save(user);
+
+    }
 }
