752c53c0db06a5603edb4b9c35cc83e37cf801b1
Gautam-Rathour <gau321tam@gmail.com>
2026-04-19 12:30:50 +0530

    Bcrypt and many more.

--- Part34-Spring Security 6 Bcrypt Password Encoder/src/main/java/com/telusko/part29springsecex/service/UserService.java
+++ Part34-Spring Security 6 Bcrypt Password Encoder/src/main/java/com/telusko/part29springsecex/service/UserService.java
@@ -2,9 +2,13 @@ package com.telusko.part29springsecex.service;
 
 import com.telusko.part29springsecex.model.Users;
 import com.telusko.part29springsecex.repo.UserRepo;
+//import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
 
 @Service
 public class UserService {
@@ -12,11 +16,28 @@ public class UserService {
     @Autowired
     private UserRepo repo;
 
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private AuthenticationManager authManager;
 
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users user) {
         user.setPassword(encoder.encode(user.getPassword()));
         repo.save(user);
         return user;
+    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken(user.getUsername());
+
+        return "fail";
     }
 }
